eae385d59fa5115b07b5b035d4b348ce68098bcb
matiasmolinazamorano1 <mat.molinaz@duocuc.cl>
2024-06-26 11:30:39 -0400

    Update Evaluacion_prueba3 (1).py
    editar archivo

--- Evaluacion_prueba3 (1).py
+++ Evaluacion_prueba3 (1).py
@@ -12,8 +12,9 @@ def agregar_estudiante():
         nom=input("Ingrese el nombre del estudiante: ");
         edad=int(input("Ingrese la edad del estudiante: "));
         curso=input("Ingrese el curso del estudiante: ");
-        promedio=int(input("Ingrese el promedio del estudiante: "));
-        matriz.append(F"|",nom,"|",edad,"|",curso,"|",promedio,"|")
+        promedio=input("Ingrese el promedio del estudiante: ");
+        fila=["|",nom,"|",edad,"|",curso,"|",promedio,"|"]
+        matriz.append(fila)
         print("¿Desea repetir el registro? (ENTER para continuar) (SI para salir)")
         pregunta=input("")
         if pregunta=="SI":
@@ -25,6 +26,27 @@ def ver_productos():
     print (lista_estudiantes);
     for x in matriz:
         print (x)
+
+
+
+
+# guardar archivo
+def guardararchivo():
+ import csv
+
+ with open('nuevo_archivolol.csv', 'w', newline='', encoding='utf-8') as archivo_csv:
+    escritor_csv = csv.writer(archivo_csv)
+
+    escritor_csv.writerow(lista_estudiantes)
+
+    escritor_csv.writerows(matriz)
+    print ("Se creo correctamente el archivo .csv"); 
+
+ import csv
+ with open('nuevo_archivolol.csv', 'r', newline='', encoding='utf-8') as archivo_csv:
+    lector_csv = csv.reader (archivo_csv);
+    for x in lector_csv:
+        print (x) 
 
 
 
